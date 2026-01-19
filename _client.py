ca96735491897d7f8c1b8dda20666e45d83b5d26
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-19 08:06:45 -0300

    Create _client.py

---
+++ _client.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from supabase import create_client
+
+@st.cache_resource
+def get_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_ANON_KEY"]
+    return create_client(url, key)
