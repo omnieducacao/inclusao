7036eefa7dd010937f5cff3fbe1d98237d5896d2
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-20 17:54:36 -0300

    Update 2_PAE.py

--- pages/2_PAE.py
+++ pages/2_PAE.py
@@ -1,5 +1,9 @@
 import streamlit as st
-from ui_nav import render_topbar_nav
+from ui_nav import render_omnisfera_nav
+
+st.set_page_config(page_title="Omnisfera | PAEE", layout="wide")
+
+render_omnisfera_nav(active="paee")
 
 
 render_topbar_nav(active="paee")
