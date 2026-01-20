393d6a39df4a414636f3e0f87c1021ffee09169a
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-20 13:49:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
-import home_portal  # noqa: F401
+import importlib.util
+
+spec = importlib.util.spec_from_file_location("home_module", "home_portal.py")
+mod = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mod)
