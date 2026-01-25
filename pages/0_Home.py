cc4209bfa60d9c81562375b4be4d76b939f274ca
amorimqueiroz-boop <amorimqueiroz@gmail.com>
2026-01-25 06:46:31 -0300

    Update 0_Home.py

--- pages/0_Home.py
+++ pages/0_Home.py
@@ -17,7 +17,7 @@ st.set_page_config(
     page_title="Omnisfera - Plataforma de Inclusão Educacional",
     page_icon="🌐" if not os.path.exists("omni_icone.png") else "omni_icone.png",
     layout="wide",
-    initial_sidebar_state="auto",
+    initial_sidebar_state="collapsed",
     menu_items=None
 )
 
